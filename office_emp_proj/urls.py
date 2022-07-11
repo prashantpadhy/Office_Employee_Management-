e731c33de7be59e341364bb505e7712056a31388
Prashant Padhy <prashantpadhy21@gmail.com>
2022-07-11 17:53:32 +0530

    last cmt

--- office_emp_proj/urls.py
+++ office_emp_proj/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import  path, include
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.urls import  re_path
 from django.views.static import serve
 
 
@@ -26,8 +26,8 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('emp_app.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
+        re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+        re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
 
 urlpatterns= urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
