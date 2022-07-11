b020eade5815b5054a52e523b3a24570e0f02c09
Prashant Padhy <prashantpadhy21@gmail.com>
2022-07-11 16:46:12 +0530

    deployement

--- office_emp_proj/urls.py
+++ office_emp_proj/urls.py
@@ -13,10 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from urllib.request import urlretrieve
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import  path, include
+from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.static import serve
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('emp_app.urls'))
+    path('', include('emp_app.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
